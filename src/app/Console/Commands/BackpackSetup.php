3c2f0f576dd9ed890fab61344b8add025ecdc303
Robb Fountain <robb@131studios.com>
2018-03-09 13:55:07 -0500

    added checks for model traits

--- src/app/Console/Commands/BackpackSetup.php
+++ src/app/Console/Commands/BackpackSetup.php
@@ -8,9 +8,11 @@ use Spatie\Permission\Models\Role;
 
 /**
  * Class PortalInstall
+ *
  * @package App\Console\Commands
  */
-class BackpackSetup extends Command {
+class BackpackSetup extends Command
+{
 
     /**
      * The name and signature of the console command.
@@ -31,7 +33,7 @@ class BackpackSetup extends Command {
      * Create a new command instance.
      *
      */
-    public function __construct()
+    public function __construct ()
     {
         parent::__construct();
     }
@@ -41,16 +43,52 @@ class BackpackSetup extends Command {
      *
      * @return mixed
      */
-    public function handle()
+    public function handle ()
     {
-        $this->createRoles();
-        $this->createUser();
+        if ($this->validateModels()) {
+            $this->createRolesAndPermissions();
+            $this->createUser();
+        }
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function validateModels ()
+    {
+        return in_array($this->traits(), class_uses(User::class));
+    }
+
+    /**
+     * @return array
+     */
+    public function traits ()
+    {
+        return [
+            'Backpack\CRUD\CrudTrait',
+            'Spatie\Permission\Traits\HasRoles',
+        ];
+    }
+
+    /**
+     *  Create Portal Roles
+     */
+    public function createRolesAndPermissions ()
+    {
+        if (!Role::where('name', 'administrator')->exists()) {
+            Role::create(['name' => 'administrator']);
+        }
+
+        if (!Permission::where(['name', 'Access Admin Panel'])->exists()) {
+            Permission::create(['name' => 'Access Admin Panel']);
+        }
     }
 
     /**
      * Creates a new User
      */
-    public function createUser()
+    public function createUser ()
     {
         User::create($this->getInputs())->assignRole('administrator');
     }
@@ -58,23 +96,12 @@ class BackpackSetup extends Command {
     /**
      * Get user inputs
      */
-    public function getInputs()
+    public function getInputs ()
     {
         return [
-            'name' => $this->ask('Full name of Administrator'),
-            'email' => $this->ask('Email address of Administrator'),
+            'name'     => $this->ask('Full name of Administrator'),
+            'email'    => $this->ask('Email address of Administrator'),
             'password' => bcrypt($this->ask('Password for Administrator Account')),
         ];
-    }
-
-    /**
-     *  Create Portal Roles
-     */
-    public function createRoles()
-    {
-        if (!Role::where('name', 'administrator')->exists())
-        {
-            Role::create(['name' => 'administrator']);
-        }
     }
 }
