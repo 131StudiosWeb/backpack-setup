920f1c492d182e859ab9f7702b10cc104bdc21ff
Robb <Robb@131studios.com>
2018-03-08 21:40:06 -0500

    remove unused files

--- src/BackpackSetupServiceProvider.php
+++ src/BackpackSetupServiceProvider.php
@@ -3,14 +3,7 @@
 namespace onethirtyone\backpacksetup;
 
 use Illuminate\Support\ServiceProvider;
-use onethirtyone\scifs\App\Classes\Data;
-use onethirtyone\scifs\app\Classes\Http;
-use onethirtyone\scifs\App\Classes\ScifsService;
-use onethirtyone\scifs\App\Console\Commands\CreateAdmin;
-use onethirtyone\scifs\App\Console\Commands\DeleteUser;
-use onethirtyone\scifs\App\Console\Commands\PortalCounts;
-use onethirtyone\scifs\App\Console\Commands\PortalInstall;
-use onethirtyone\scifs\App\Providers\ComposerServiceProvider;
+
 
 class BackpackSetupServiceProvider extends ServiceProvider {
     /**
@@ -20,7 +13,8 @@ class BackpackSetupServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        if($this->app->runningInConsole()) {
+        if ($this->app->runningInConsole())
+        {
             $this->commands([
                 BackpackSetup::class
             ]);
